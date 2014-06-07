eaf122300159ebc577a9bae6cfbe418b17ff44cf
modoranu <marius@modoranu.com>
2014-06-07 14:46:35 +0200

    Create es.php
    Informal spanish translation

---
+++ languages/es.php
@@ -0,0 +1,27 @@
+<?php
+
+$spanish = array(
+
+	'dropzone:response_error' => 'Lo sentimos, hubo un error desconocido. Intenta de nuevo después de refrescar la página',
+	'dropzone:default_message' => '<strong>Arrastrar y soltar tus fotos aqu√≠</strong><br /><span>o haz clic para seleccionarlas desde tu ordenador</span>',
+	'dropzone:remove_file' => 'Eliminar',
+	'dropzone:max_files_exceeded' => 'Has llegado numero máximo de archivos que puedes subir a la vez',
+	'dropzone:fallback_message' => 'Tu navegador no es compatible con la función "arrastrar y soltar" archivos</i>',
+	'dropzone:fallback_text' => 'Por favor, utiliza el método antíguo para las subidas',
+	'dropzone:invalid_filetype' => 'Los archivos de este tipo no están permitidas',
+	'dropzone:file_too_big' => 'El archivo pesa demasiado',
+	'dropzone:cancel_upload' => 'Cancelar subida',
+	'dropzone:cancel_upload_confirmation' => '¿Estás seguro de que quieres cancelar la subida?',
+
+	// Upload errors
+	'upload:success' => 'Archivo subido con éxito',
+	'upload:error:no_file' => 'No has elegido el erchivo correctamente. Intenta de nuevo',
+	'upload:error:file_size' => 'Has superado el tamaño máximo permitido del archivo',
+	'upload:error:unknown' => 'Lo sentimos, hubo un error desconocido. Intenta de nuevo después de refrescar la página',
+
+
+);
+
+add_translation('es', $spanish);
+
+    // Informal spanish translation - githtub.com/modoranu 
